9fc317cff67f3740a0629bb55f8555122b5798c0
wataru okada <wataru.okada@flect.co.jp>
2020-11-18 15:29:19 +0900

    WIP whiteboard

--- frontend/src/containers/CustomVideoTileGrid/CustomContentShare.tsx
+++ frontend/src/containers/CustomVideoTileGrid/CustomContentShare.tsx
@@ -1,7 +1,7 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { useEffect, useRef, HTMLAttributes, forwardRef } from 'react';
+import React, { useEffect, useRef, HTMLAttributes, forwardRef, MutableRefObject } from 'react';
 import { BaseSdkProps } from 'amazon-chime-sdk-component-library-react/lib/components/sdk/Base';
 import { useAudioVideo, useContentShareState, VideoTile } from 'amazon-chime-sdk-component-library-react';
 import styled from 'styled-components';
@@ -11,57 +11,79 @@ import { CustomStyledVideoTile } from './CustomStyledVideoTile';
 
 type ObjectFit = 'contain' | 'cover' | 'fill' | 'none' | 'scale-down';
 
+
+class SharedContentDrawer {
+
+    private static _instance: SharedContentDrawer
+    public static getInstance(): SharedContentDrawer {
+        if (!this._instance) {
+            this._instance = new SharedContentDrawer()
+        }
+        return this._instance
+    }
+
+    private _videoRef: MutableRefObject<HTMLVideoElement | null> | null = null
+    private _canvasRef: MutableRefObject<HTMLCanvasElement | null> | null = null
+    private _drawing: boolean = false
+    set videoRef(val: MutableRefObject<HTMLVideoElement | null>) {
+        this._videoRef = val
+        this._videoRef.current!.play()
+    }
+    set canvasRef(val: MutableRefObject<HTMLCanvasElement | null>) { this._canvasRef = val }
+    set drawing(val: boolean) {
+        if (this._drawing !== val) {
+            this._drawing = val
+            this.startDrawing()
+        }
+    }
+
+    private startDrawing = () => {
+        if (this._videoRef && this._canvasRef && this._videoRef.current && this._drawing) {
+            try{
+                this._canvasRef.current!.width = this._videoRef.current!.videoWidth
+                this._canvasRef.current!.height = this._videoRef.current!.videoHeight
+                this._canvasRef.current!.getContext("2d")!.drawImage(this._videoRef.current!, 0, 0, this._canvasRef.current!.width, this._canvasRef.current!.height)
+                const now = performance.now()
+                this._canvasRef.current!.getContext("2d")!.fillText("" + now, 100, 200)
+            }catch(exception){
+                console.log(exception)
+            }
+            requestAnimationFrame(this.startDrawing)
+        }
+    }
+
+}
+
 export interface VideoTileProps
     extends Omit<HTMLAttributes<HTMLDivElement>, 'css'>,
     BaseProps {
-    /** The name to show on the video tile */
     nameplate?: string | null;
-    /** Specifies which CSS object-fit value to apply to the VideoTile so that it resizes to fit its container  */
     objectFit?: ObjectFit;
 }
 
 export const CustomVideoTile = forwardRef(
     (props: VideoTileProps, ref: React.Ref<HTMLVideoElement>) => {
         const { tag, className, nameplate, ...rest } = props;
-        const canvasEl = useRef<HTMLCanvasElement | null>(null);
-
         const audioVideo = useAudioVideo();
         const { tileId } = useContentShareState();
+        const canvasEl = useRef<HTMLCanvasElement | null>(null);
         const videoEl = useRef<HTMLVideoElement | null>(null);
 
-        console.log("--->", ref)
-
-        useEffect(() => {
-
-        });
-
-        // setInterval(()=>{console.log("width^^",videoEl.current!.width)},1000)
-        // setInterval(()=>{console.log("width^_^",canvasEl.current!.width)},1000)
-        // setInterval(()=>{console.log("width^__^", audioVideo!.getRemoteVideoSources())},1000)
-        
         useEffect(() => {
             if (!audioVideo || !videoEl.current || !tileId) {
                 return;
             }
 
-
-            if(tileId){
-                const state = audioVideo!.getVideoTile(tileId)?.state()
-                canvasEl.current!.width = state!.videoStreamContentWidth!
-                canvasEl.current!.height = state!.videoStreamContentHeight!
-                console.log("width^_.._^1:", state!.videoStreamContentWidth, state!.videoStreamContentHeight)
+            if (tileId) {
+                const drawer = SharedContentDrawer.getInstance()
+                drawer.videoRef = videoEl
+                drawer.canvasRef = canvasEl
+                drawer.drawing = true
             }
-                audioVideo.bindVideoElement(tileId, videoEl.current);
-            // canvasEl.current!.getContext("2d")!.fillText(`${now}`, 10, 10)
-
-            if (!canvasEl.current) {
-                return;
-            }
-            const now = performance.now()
-            canvasEl.current?.getContext("2d")?.fillText(`${now}`, 10, 10)
-            console.log("-----", canvasEl.current!?.width, canvasEl.current!?.height)
-                       
+            audioVideo.bindVideoElement(tileId, videoEl.current);
             return () => {
+                const drawer = SharedContentDrawer.getInstance()
+                drawer.drawing = false
                 const tile = audioVideo.getVideoTile(tileId);
                 if (tile) {
                     audioVideo.unbindVideoElement(tileId);
@@ -76,7 +98,7 @@ export const CustomVideoTile = forwardRef(
                 data-testid="video-tile"
                 {...rest}
             >
-                <video ref={videoEl} className="ch-video" />
+                <video ref={videoEl} className="ch-video" /> 
                 <canvas ref={canvasEl} />
                 {nameplate && (
                     <header className="ch-nameplate">
@@ -88,46 +110,17 @@ export const CustomVideoTile = forwardRef(
     }
 );
 
-
-
 interface Props extends BaseSdkProps { }
 
-
-
 export const CustomContentShare: React.FC<Props> = ({ className, ...rest }) => {
-    const audioVideo = useAudioVideo();
     const { tileId } = useContentShareState();
-    const videoEl = useRef<HTMLVideoElement | null>(null);
-    const canvasEl = useRef<HTMLCanvasElement | null>(null);
-
-    // useEffect(() => {
-    //     if (!audioVideo || !videoEl.current || !tileId) {
-    //         return;
-    //     }
-
-    //     audioVideo.bindVideoElement(tileId, videoEl.current);
-    //     // canvasEl.current!.getContext("2d")!.fillText(`${now}`, 10, 10)
-    //     return () => {
-    //         const tile = audioVideo.getVideoTile(tileId);
-    //         if (tile) {
-    //             audioVideo.unbindVideoElement(tileId);
-    //         }
-    //     };
-    // }, [audioVideo, tileId]);
-    // console.log("------->", rest)
-    // console.log("------->", className)
-
-    // const now = performance.now()
-
 
     return tileId ? (
         <>
             <ContentTile
                 objectFit="contain"
                 className={className || ''}
-                // {...rest}
-                ref={videoEl}
-
+                {...rest}
             />
         </>
     ) : null;
