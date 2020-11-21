8039a652adc492f68903a64351764ba148e7b0fa
wataru okada <wataru.okada@flect.co.jp>
2020-11-22 03:11:09 +0900

    WIP websocket: authorization is done!

--- backend/lib/lambda/message.js
+++ backend/lib/lambda/message.js
@@ -2,6 +2,7 @@
 var utils = require('./utils')
 var AWS = require('aws-sdk');
 const chime = new AWS.Chime({ region: 'us-east-1' });
+var ddb = new AWS.DynamoDB();
 
 const generatePolicy = (principalId, effect, resource, context) => {
     const authResponse = {};
@@ -64,20 +65,123 @@ exports.connect = async (event, context, callback) => {
     console.log(event)
     console.log(context)
     console.log(callback)
-    const response = utils.getResponseTemplate()
-    callback(null, response)
+    const oneDayFromNow  = Math.floor(Date.now() / 1000) + 60 * 60 * 24;
+    try {
+        const meetingId  = event.queryStringParameters.meetingId
+        const attendeeId = event.queryStringParameters.attendeeId
+        console.log("meetingId:", meetingId)
+        console.log("attendeeId", attendeeId)
+        const res = await ddb.putItem({
+                TableName : process.env.CONNECTION_TABLE_NAME,
+                Item      : {
+                    MeetingId    : { S: meetingId },
+                    AttendeeId   : { S: attendeeId },
+                    ConnectionId : { S: event.requestContext.connectionId },
+                    TTL          : { N: `${oneDayFromNow}` }
+                }
+            })
+            .promise();
+        console.log("update res", res)
+    } catch (e) {
+        console.error(`error connecting: ${e.message}`);
+        return {
+            statusCode: 500,
+            body: `Failed to connect: ${JSON.stringify(e)}`
+        };
+    }
+    return { statusCode: 200, body: 'Connected.' };
 }
+
+
 exports.disconnect = async (event, context, callback) => {
     console.log(event)
     console.log(context)
     console.log(callback)
-    const response = utils.getResponseTemplate()
-    callback(null, response)
+    try {
+        const meetingId  = event.requestContext.authorizer.meetingId
+        const attendeeId = event.requestContext.authorizer.attendeeId
+        console.log(meetingId, attendeeId)
+        await ddb.deleteItem({
+                TableName : process.env.CONNECTION_TABLE_NAME,
+                Key       : {
+                    MeetingId  : { S: meetingId  },
+                    AttendeeId : { S: attendeeId }
+                }
+            })
+            .promise();
+    } catch (err) {
+        console.error(`error : ${err.message}`);
+        return {
+            statusCode: 500,
+            body: `Failed to disconnect: ${JSON.stringify(err)}`
+        };
+    }
+    return { statusCode: 200, body: 'Disconnected.' };
 }
+
+
 exports.message = async (event, context, callback) => {
     console.log(event)
     console.log(context)
     console.log(callback)
-    const response = utils.getResponseTemplate()
-    callback(null, response)
-}
+    console.log('sendmessage event:', JSON.stringify(event, null, 2));
+
+    // Gather the information of attendees
+    let attendees = {};
+    try {
+        attendees = await ddb.query({
+                ExpressionAttributeValues: {
+                    ':meetingId': { S: event.requestContext.authorizer.meetingId }
+                },
+                KeyConditionExpression : 'MeetingId = :meetingId',
+                ProjectionExpression   : 'ConnectionId, AttendeeId',
+                TableName              : process.env.CONNECTION_TABLE_NAME,
+            })
+            .promise();
+    } catch (e) {
+        console.log('Query error:', e);
+        return { statusCode: 500, body: e.stack };
+    }
+    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion : '2018-11-29',
+        endpoint   : `${event.requestContext.domainName}/${event.requestContext.stage}`
+    });
+    
+    console.log("DATA:",event.body)
+    const body = JSON.parse(event.body)
+    const targetId = body.targetId
+    const private  = body.private
+
+    const postCalls = attendees.Items.map(async connection => {
+        const connectionId = connection.ConnectionId.S
+        const attendeeId   = connection.AttendeeId.S
+        if(private !==true || attendeeId === targetId){
+            try {
+                await apigwManagementApi
+                    .postToConnection({ ConnectionId: connectionId, Data: JSON.stringify(body)})
+                    .promise();
+            } catch (e) {
+                if (e.statusCode === 410) {
+                    console.log(`found stale connection, skipping ${connectionId}`);
+                } else {
+                    console.error(
+                        `error posting to connection ${connectionId}: ${e.message}`
+                    );
+                }
+            }
+        }
+    });
+
+    try {
+        await Promise.all(postCalls);
+    } catch (e) {
+        console.error(`failed to post: ${e.message}`);
+        return { statusCode: 500, body: e.stack };
+    }
+    
+    body.done              = true
+    body.content.fileParts = "" // reduce trafic
+
+    return { statusCode: 200, body: JSON.stringify(body) };}
+
+    
