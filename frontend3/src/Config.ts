119655a4cf6f5289fbcba3f2ae4875ec16d8b5b3
wataru <wataru.okada@flect.co.jp>
2021-04-20 07:16:06 +0900

    update

--- frontend3/src/Config.ts
+++ frontend3/src/Config.ts
@@ -9,6 +9,6 @@ export const awsConfiguration = {
 export const BASE_URL = RestAPIEndpoint
 // export const DEFAULT_USERID = "mail2wokada@gmail.com"
 // export const DEFAULT_PASSWORD = "password"
-export const DEFAULT_USERID = null
-export const DEFAULT_PASSWORD = null
+export const DEFAULT_USERID = ""
+export const DEFAULT_PASSWORD = ""
 
