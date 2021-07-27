b4d0f775eb082a77c0c33e64d57716549abeb0e0
wataru <wataru.okada@flect.co.jp>
2021-07-27 14:25:07 +0900

    update

--- backend/bin/config.ts
+++ backend/bin/config.ts
@@ -1,4 +1,9 @@
-export const BACKEND_STACK_NAME = 'xxxxx'
+export const BACKEND_STACK_NAME = 'xxxxxxxxx'
+export const USE_CDN = true
+
+
+/////////////////// //////////////////////////
+// Don't change below (only for developper) //
+//////////////////////////////////////////////
 export const FRONTEND_LOCAL_DEV = false
 export const USE_DOCKER = false
-export const USE_CDN = true
