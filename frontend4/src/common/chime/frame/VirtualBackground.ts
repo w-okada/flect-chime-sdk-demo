c40e99172919b1f27dd5eca88961b2399aad23dd
wataru <wataru.okada@flect.co.jp>
2021-07-29 11:10:49 +0900

    update

--- frontend4/src/common/chime/frame/VirtualBackground.ts
+++ frontend4/src/common/chime/frame/VirtualBackground.ts
@@ -30,16 +30,7 @@ export class VirtualBackground implements VideoFrameProcessor {
     GMTfjs = new VirtualBackgroundGoogleMeetTFJS()
     GMTflite = new VirtualBackgroundGoogleMeetTFLite()
 
-
-
-    canvasFront = document.createElement('canvas')
-    canvasFrontResized = document.createElement("canvas")
     canvasBackground = document.createElement("canvas")
-
-    // inputCanvas = document.createElement("canvas")
-    personCanvas = document.createElement("canvas")
-    personMaskCanvas = document.createElement("canvas")
-    lightWrapCanvas = document.createElement("canvas")
 
     private config: VirtualBackgroundConfig = {
         frontPositionX: 0, // ratio (position and size of front image)
@@ -59,14 +50,8 @@ export class VirtualBackground implements VideoFrameProcessor {
     // Video Processing API RSC  //
     //////////////////////////////
     targetCanvas = document.createElement('canvas')
-    targetCanvasCtx = this.targetCanvas!.getContext('2d')
     canvasVideoFrameBuffer = new CanvasVideoFrameBuffer(this.targetCanvas!);
 
-    /////////////////////
-    // WorkerManagers  //
-    /////////////////////
-
-    
     ////////////////////////////
     // constructor & destory ///
     ////////////////////////////
@@ -80,7 +65,6 @@ export class VirtualBackground implements VideoFrameProcessor {
     }
 
     async destroy() {
-        this.targetCanvasCtx = null;
         this.canvasVideoFrameBuffer.destroy();
         return;
     }
@@ -132,28 +116,13 @@ export class VirtualBackground implements VideoFrameProcessor {
                 // let result: any
                 switch (this.config.type) {
                     case "BodyPix":
-                        // result = await this.bodyPixManager!.predict(canvas, this.bodyPixParams!)
-                        // if(this.bodyPixModelReady){
-                            // const result = await this.bodyPixManager!.predict(canvas, this.bodyPixParams!)
-                            // this.convert_bodypix(canvas, this.canvasBackground, result, this.config)
-                            this.BodyPix.predict(canvas, this.canvasBackground, this.targetCanvas, this.config)
-                        // }
+                        this.BodyPix.predict(canvas, this.canvasBackground, this.targetCanvas, this.config)
                         break
                     case "GoogleMeet":
-                        // result = await this.googleMeetManager!.predict(canvas, this.googleMeetParams!)
-                        // if(this.googlemeetModelReady){
-                            // const result = await this.googlemeetManager!.predict(canvas, this.googlemeetParams!)
-                            // this.convert_googlemeet(canvas, this.canvasBackground, result, this.config)
-                            this.GMTfjs.predict(canvas, this.canvasBackground, this.targetCanvas, this.config)
-                        // }
+                        this.GMTfjs.predict(canvas, this.canvasBackground, this.targetCanvas, this.config)
                         break
                     case "GoogleMeetTFLite":
-                        // result = await this.googleMeetManager!.predict(canvas, this.googleMeetParams!)
-                        // if(this.googlemeetTFLiteModelReady){
-                            // const result = await this.googlemeetTFLiteManager!.predict(canvas, this.googlemeetTFLiteParams!)
-                            // this.convert_googlemeet_tflite(canvas, this.canvasBackground, result, this.config)
-                            this.GMTflite.predict(canvas, this.canvasBackground, this.targetCanvas, this.config)
-                        // }
+                        this.GMTflite.predict(canvas, this.canvasBackground, this.targetCanvas, this.config)
                         break
                     default:
                         this.convert_none(canvas)
@@ -165,11 +134,6 @@ export class VirtualBackground implements VideoFrameProcessor {
         buffers[0] = this.canvasVideoFrameBuffer;
         return Promise.resolve(buffers)
     }
-
-
-
-
-
 
 
     convert_none = (foreground: HTMLCanvasElement) => {
