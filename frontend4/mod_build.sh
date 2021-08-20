6f2d6e429a0a78fb62faaed3ea6ebdf92d00f277
wataru <wataru.okada@flect.co.jp>
2021-08-20 21:35:41 +0900

    update

--- frontend4/mod_build.sh
+++ frontend4/mod_build.sh
@@ -1,2 +1,3 @@
 #cd ../flect-chime-lib/ && npm run build && cd - && cp -r ../flect-chime-lib/dist/* node_modules/\@dannadori/flect-chime-lib/dist/
-cd ../flect-chime-lib-rollup/ && npm run build && cd - && cp -r ../flect-chime-lib-rollup/dist/* node_modules/\@dannadori/flect-chime-lib/dist/
+#cd ../flect-chime-lib-rollup/ && npm run build && cd - && cp -r ../flect-chime-lib-rollup/dist/* node_modules/\@dannadori/flect-chime-lib/dist/
+cd ../flect-amazon-chime-lib  && npm run build && cd - && cp -r ../flect-amazon-chime-lib/dist/* node_modules/\@dannadori/flect-amazon-chime-lib/dist/
