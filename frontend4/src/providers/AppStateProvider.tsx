4b27d1419f5dc284d5acd406e205de03eb09a7d0
wataru <wataru.okada@flect.co.jp>
2021-09-25 15:30:59 +0900

    update

--- frontend4/src/providers/AppStateProvider.tsx
+++ frontend4/src/providers/AppStateProvider.tsx
@@ -76,8 +76,8 @@ export const AppStateProvider = ({ children }: Props) => {
     const [ lastUpdateTime, setLastUpdateTime ] = useState(0) // eslint-disable-line
 
     const cognitoClient = useMemo(()=>{
-//        return new CognitoClient(UserPoolId, UserPoolClientId)
-        return new CognitoClient(UserPoolId, UserPoolClientId, "mail2wokada@gmail.com", "test22")
+        return new CognitoClient(UserPoolId, UserPoolClientId)
+        // return new CognitoClient(UserPoolId, UserPoolClientId, "mail2wokada@gmail.com", "test22")
     },[])
 
     /////////
