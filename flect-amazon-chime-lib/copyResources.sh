2da55f4d2d0b461ab0f507f604022c8a4cdeedb3
Wataru Okada <wataru.okada@flect.co.jp>
2021-12-07 22:15:36 +0900

    update

--- flect-amazon-chime-lib/copyResources.sh
+++
@@ -1,6 +0,0 @@
-# FFPEG
-cp node_modules/\@ffmpeg/core/dist/ffmpeg-core.wasm resources/ffmpeg/
-cp node_modules/\@ffmpeg/core/dist/ffmpeg-core.worker.js resources/ffmpeg/
-cp node_modules/\@ffmpeg/core/dist/ffmpeg-core.js resources/ffmpeg/
-
-
